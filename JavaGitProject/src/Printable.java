274a3225bba2c13fcaeeee4ba2edcefc908e360e
Archana-17-P <archana17@hotmail.co.uk>
2021-05-10 13:38:02 +0100

    Simple java program

---
+++ JavaGitProject/src/Printable.java
@@ -0,0 +1,10 @@
+
+/* An interface is a collection of constant members and abstract method
+ * We can not create object of an interface
+ * WE can achieve multiple inheritance through interfaces
+ */
+public interface Printable {
+int interfaceX=15;
+public void print();
+
+}
